e3a83def43a4036624189af196c096a914ceb7f8
Praveen <pro.prkhere@hotmail.com>
2026-01-12 21:42:27 +0530

    fixed home page dependencies

--- rma-frontend/src/pages/Home.jsx
+++ rma-frontend/src/pages/Home.jsx
@@ -1,3 +1,16 @@
+import Hero from '../components/home/Hero'
+import Challenges from '../components/home/Challenges'
+import Solution from '../components/home/Solution'
+import ProgramEnhancements from '../components/home/ProgramEnhancements'
+import ExpectedImpact from '../components/home/ExpectedImpact'
+import Phase1 from '../components/home/Phase1'
+import Phase2 from '../components/home/Phase2'
+import Phase3 from '../components/home/Phase3'
+import Phase4 from '../components/home/Phase4'
+import DecisionPoint from '../components/home/DecisionPoint'
+import Phase5 from '../components/home/Phase5'
+import CompleteFlow from '../components/home/CompleteFlow'
+
 function Home() {
     return (
         <>
